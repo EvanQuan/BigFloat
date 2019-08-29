5e524e24782ece41e8c4a4555e6fa47dfa5c6fc5
Evan Quan <evanlquan@gmail.com>
2019-08-28 19:38:33 -0600

    Shorten namespaces

--- BigFloat/Implementations/BigFloat.cs
+++ BigFloat/Implementations/BigFloat.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Text;
 using System.Numerics;
 
-namespace Com.Github.EvanQuan.BigFloatingPoint.Implementations
+namespace BigFloatingPoint.Implementations
 {
     /// <summary>
     /// Represents an arbitrarily large signed floating point number.
