d14375c356714e5384948e8b3836f6fd1650b18e
Evan Quan <evanlquan@gmail.com>
2019-08-28 20:20:53 -0600

    Add float tests

--- BigFloat.UnitTests/UsingInstanceMethods/WhenConvertingToString.cs
+++ BigFloat.UnitTests/UsingInstanceMethods/WhenConvertingToString.cs
@@ -10,14 +10,31 @@ namespace BigFloatingPoint.UnitTests.UsingInstanceMethods
         [InlineData(1, "1")]
         [InlineData(-1, "-1")]
         [InlineData(0.1, "0.1")]
-        [InlineData(0.00001, "0.00001")]
-        [InlineData(0.000001, "0.000001")]
-        [InlineData(-0.1, "-0.1")]
-        [InlineData(-0.00001, "-0.00001")]
-        [InlineData(-0.000001, "-0.000001")]
+        [InlineData(0.00001d, "0.00001")]
+        [InlineData(0.000001d, "0.000001")]
+        [InlineData(-0.1d, "-0.1")]
+        [InlineData(-0.00001d, "-0.00001")]
+        [InlineData(-0.000001d, "-0.000001")]
         [InlineData(double.MaxValue, "0")]
         [InlineData(double.MinValue, "0")]
         public void ShouldWorkForDouble(double value, string expected)
+        {
+            Assert.Equal(expected, new BigFloat(value).ToString());
+        }
+
+        [Theory]
+        [InlineData(0, "0")]
+        [InlineData(1, "1")]
+        [InlineData(-1, "-1")]
+        [InlineData(0.1, "0.1")]
+        [InlineData(0.00001f, "0.00001")]
+        [InlineData(0.000001f, "0.000001")]
+        [InlineData(-0.1f, "-0.1")]
+        [InlineData(-0.00001f, "-0.00001")]
+        [InlineData(-0.000001f, "-0.000001")]
+        [InlineData(float.MaxValue, "0")]
+        [InlineData(float.MinValue, "0")]
+        public void ShouldWorkForFloat(float value, string expected)
         {
             Assert.Equal(expected, new BigFloat(value).ToString());
         }
