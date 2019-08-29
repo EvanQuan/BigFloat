b0cb47b4ee7fb5e322f81b376020d40384c6cc9d
Evan Quan <evanlquan@gmail.com>
2019-08-28 20:01:11 -0600

    Add ToString tests

--- BigFloat.UnitTests/UsingInstanceMethods/WhenConvertingToString.cs
+++ BigFloat.UnitTests/UsingInstanceMethods/WhenConvertingToString.cs
@@ -3,7 +3,35 @@ using Xunit;
 
 namespace BigFloatingPoint.UnitTests.UsingInstanceMethods
 {
-    class WhenConvertingToString
+    public class WhenConvertingToString
     {
+        [Theory]
+        [InlineData(0, "0")]
+        [InlineData(1, "1")]
+        [InlineData(-1, "-1")]
+        [InlineData(0.1, "0.1")]
+        [InlineData(0.00001, "0.00001")]
+        [InlineData(0.000001, "0.000001")]
+        [InlineData(-0.1, "-0.1")]
+        [InlineData(-0.00001, "-0.00001")]
+        [InlineData(-0.000001, "-0.000001")]
+        [InlineData(double.MaxValue, "0")]
+        [InlineData(double.MinValue, "0")]
+        public void ShouldWorkForDouble(double value, string expected)
+        {
+            Assert.Equal(expected, new BigFloat(value).ToString());
+        }
+
+        [Theory]
+        [InlineData(0, "0")]
+        [InlineData(1, "1")]
+        [InlineData(-1, "-1")]
+        [InlineData(int.MaxValue, "2147483647")]
+        [InlineData(int.MinValue, "-2147483648")]
+        public void ShouldWorkForInt(int value, string expected)
+        {
+            Assert.Equal(expected, new BigFloat(value).ToString());
+        }
+
     }
 }
