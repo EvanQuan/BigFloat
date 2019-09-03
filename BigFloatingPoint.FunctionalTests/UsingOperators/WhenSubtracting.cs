ab67743690ce86b25e27c72cdc8f47d1cdb615a2
Evan Quan <evanlquan@gmail.com>
2019-09-02 20:25:41 -0600

    Add extra subtracting tests.

--- BigFloatingPoint.FunctionalTests/UsingOperators/WhenSubtracting.cs
+++ BigFloatingPoint.FunctionalTests/UsingOperators/WhenSubtracting.cs
@@ -6,24 +6,33 @@ namespace BigFloatingPoint.FunctionalTests.UsingOperators
     public class WhenSubtracting
     {
         [Theory]
-        [InlineData(0, 0, 0)]
-        [InlineData(1, 1, 0)]
-        [InlineData(-1, -1, 0)]
-        [InlineData(-1.5, -1.5, 0)]
-        [InlineData(int.MaxValue, int.MaxValue, 0)]
-        [InlineData(-int.MaxValue, -int.MaxValue, 0)]
-        [InlineData(float.MaxValue, float.MaxValue, 0)]
-        [InlineData(-float.MaxValue, -float.MaxValue, 0)]
-        [InlineData(double.MaxValue, double.MaxValue, 0)]
-        [InlineData(-double.MaxValue, -double.MaxValue, 0)]
-        [InlineData(1, -1, 2)]
-        [InlineData(-1, 1, -2)]
-        [InlineData(1.5, -1.5, 3.0)]
-        [InlineData(-1.5, 1.5, -3.0)]
+        [InlineData("0", "0", "0")]
+        [InlineData("1", "1", "0")]
+        [InlineData("-1", "-1", "0")]
+        [InlineData("-1.5", "-1.5", "0")]
+        [InlineData("1", "-1", "2")]
+        [InlineData("-1", "1", "-2")]
+        [InlineData("1.5", "-1.5", "3.0")]
+        [InlineData("-1.5", "1.5", "-3.0")]
+        [InlineData(ConstantStrings.MaxInt, ConstantStrings.Negative + ConstantStrings.MaxInt, ConstantStrings.MaxIntTimes2)]
+        [InlineData(ConstantStrings.Negative + ConstantStrings.MaxInt, ConstantStrings.MaxInt, ConstantStrings.Negative + ConstantStrings.MaxIntTimes2)]
+        [InlineData(ConstantStrings.MaxInt, "-1", ConstantStrings.MaxIntPlus1)]
+        [InlineData(ConstantStrings.MinInt, "1", ConstantStrings.MinIntMinus1)]
+        [InlineData(ConstantStrings.MaxInt, ConstantStrings.MaxInt, "0")]
+        [InlineData(ConstantStrings.MaxFloat, ConstantStrings.Negative + ConstantStrings.MaxFloat, ConstantStrings.MaxFloatTimes2)]
+        [InlineData(ConstantStrings.MinFloat, ConstantStrings.MaxFloat, ConstantStrings.MinFloatTimes2)]
+        [InlineData(ConstantStrings.MaxFloat, "-1", ConstantStrings.MaxFloatPlus1)]
+        [InlineData(ConstantStrings.MinFloat, "1", ConstantStrings.MinFloatMinus1)]
+        [InlineData(ConstantStrings.MaxFloat, ConstantStrings.MaxFloat, "0")]
+        [InlineData(ConstantStrings.MaxDouble, ConstantStrings.Negative + ConstantStrings.MaxDouble, ConstantStrings.MaxDoubleTimes2)]
+        [InlineData(ConstantStrings.MinDouble, ConstantStrings.MaxDouble, ConstantStrings.MinDoubleTimes2)]
+        [InlineData(ConstantStrings.MaxDouble, "-1", ConstantStrings.MaxDoublePlus1)]
+        [InlineData(ConstantStrings.MinDouble, "1", ConstantStrings.MinDoubleMinus1)]
+        [InlineData(ConstantStrings.MaxDouble, ConstantStrings.MaxDouble, "0")]
         public void ShouldSubtract(
-            double minuend,
-            double subtrahend,
-            double expectedDifference)
+            string minuend,
+            string subtrahend,
+            string expectedDifference)
         {
             BigFloat bigMinuend = new BigFloat(minuend);
             BigFloat bigSubtrahend = new BigFloat(subtrahend);
