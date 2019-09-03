31348ad89477e2341c8280e0093469e5cd6e95b5
Evan Quan <evanlquan@gmail.com>
2019-09-02 22:01:27 -0600

    Add more remainder tests. Failing.

--- BigFloatingPoint.FunctionalTests/UsingOperators/WhenGettingRemainder.cs
+++ BigFloatingPoint.FunctionalTests/UsingOperators/WhenGettingRemainder.cs
@@ -7,14 +7,31 @@ namespace BigFloatingPoint.FunctionalTests.UsingOperators
     public class WhenGettingRemainder
     {
         [Theory]
-        [InlineData(0.0, 1.0, 0.0)]
-        [InlineData(1.0, 1.0, 0.0)]
-        [InlineData(2.0, 1.0, 0.0)]
-        [InlineData(3.0, 2.0, 1.0)]
-        public void ShouldPreDecrement(
-            double dividend,
-            double divisor,
-            double expectedRemainder)
+        [InlineData("0.0", "1.0", "0.0")]
+        [InlineData("0.0", "-1.0", "0.0")]
+        [InlineData("1.0", "0.5", "0.0")]
+        [InlineData("1.0", "-0.5", "0.0")]
+        [InlineData("-1.0", "0.5", "0.0")]
+        [InlineData("1.0", "1.5", "1.0")]
+        [InlineData("1.0", "-1.5", "1.0")]
+        [InlineData("1.0", "2.0", "1.0")]
+        [InlineData("1.0", "0.8", "0.2")]
+        [InlineData("1.0", "-0.8", "0.2")]
+        [InlineData("2.0", "0.8", "0.4")]
+        [InlineData("2.0", "-0.8", "0.4")]
+        [InlineData("1.0", "1.0", "0.0")]
+        [InlineData("3.0", "1.0", "0.0")]
+        [InlineData("3.0", "-1.0", "0.0")]
+        [InlineData("3.0", "2.0", "1.0")]
+        [InlineData("3.0", "1.5", "0.0")]
+        [InlineData("5.0", "4.0", "1.0")]
+        [InlineData("5.0", "-4.0", "1.0")]
+        [InlineData("-5.0", "4.0", "-1.0")]
+        [InlineData("-5.0", "-4.0", "-1.0")]
+        public void ShouldGetRemainder(
+            string dividend,
+            string divisor,
+            string expectedRemainder)
         {
             BigFloat bigDividend = new BigFloat(dividend);
             BigFloat bigDivisor = new BigFloat(divisor);
@@ -28,11 +45,29 @@ namespace BigFloatingPoint.FunctionalTests.UsingOperators
         }
 
         [Theory]
-        [InlineData(0.0)]
-        [InlineData(1.0)]
-        [InlineData(-1.0)]
+        [InlineData("0.0")]
+        [InlineData("1.0")]
+        [InlineData("-1.0")]
+        [InlineData(ConstantStrings.MinIntTimes2)]
+        [InlineData(ConstantStrings.MinIntMinus1)]
+        [InlineData(ConstantStrings.MinInt)]
+        [InlineData(ConstantStrings.MaxInt)]
+        [InlineData(ConstantStrings.MaxIntPlus1)]
+        [InlineData(ConstantStrings.MaxIntTimes2)]
+        [InlineData(ConstantStrings.MinFloatTimes2)]
+        [InlineData(ConstantStrings.MinFloatMinus1)]
+        [InlineData(ConstantStrings.MinFloat)]
+        [InlineData(ConstantStrings.MaxFloat)]
+        [InlineData(ConstantStrings.MaxFloatPlus1)]
+        [InlineData(ConstantStrings.MaxFloatTimes2)]
+        [InlineData(ConstantStrings.MinDoubleTimes2)]
+        [InlineData(ConstantStrings.MinDoubleMinus1)]
+        [InlineData(ConstantStrings.MinDouble)]
+        [InlineData(ConstantStrings.MaxDouble)]
+        [InlineData(ConstantStrings.MaxDoublePlus1)]
+        [InlineData(ConstantStrings.MaxDoubleTimes2)]
         public void ShouldThrowDivideByZeroException(
-            double dividend)
+            string dividend)
         {
             Assert.Throws<DivideByZeroException>(() =>
             {
