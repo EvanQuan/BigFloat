df03990d414086c02816614505fff5cd096bdfd9
Evan Quan <evanlquan@gmail.com>
2019-09-02 20:37:25 -0600

    Add more absolute value tests.

--- BigFloatingPoint.FunctionalTests/UsingOperators/WhenGettingAbsoluteValue.cs
+++ BigFloatingPoint.FunctionalTests/UsingOperators/WhenGettingAbsoluteValue.cs
@@ -7,18 +7,24 @@ namespace BigFloatingPoint.FunctionalTests.UsingOperators
     public class WhenGettingAbsoluteValue
     {
         [Theory]
-        [InlineData(0, 0)]
-        [InlineData(1, 1)]
-        [InlineData(-1, 1)]
-        [InlineData(-1.5, 1.5)]
-        [InlineData(1.5, 1.5)]
-        [InlineData(int.MaxValue, int.MaxValue)]
-        [InlineData(-int.MaxValue, int.MaxValue)]
-        [InlineData(float.MaxValue, float.MaxValue)]
-        [InlineData(-float.MaxValue, float.MaxValue)]
-        [InlineData(double.MaxValue, double.MaxValue)]
-        [InlineData(-double.MaxValue, double.MaxValue)]
-        public void ShouldGetAbsoluteValue(double value, double expected)
+        [InlineData("0", "0")]
+        [InlineData("1", "1")]
+        [InlineData("-1", "1")]
+        [InlineData("-1.5", "1.5")]
+        [InlineData("1.5", "1.5")]
+        [InlineData(ConstantStrings.MaxInt, ConstantStrings.MaxInt)]
+        [InlineData(ConstantStrings.Negative + ConstantStrings.MaxInt, ConstantStrings.MaxInt)]
+        [InlineData(ConstantStrings.MaxFloat, ConstantStrings.MaxFloat)]
+        [InlineData(ConstantStrings.Negative + ConstantStrings.MaxFloat, ConstantStrings.MaxFloat)]
+        [InlineData(ConstantStrings.MaxDouble, ConstantStrings.MaxDouble)]
+        [InlineData(ConstantStrings.Negative + ConstantStrings.MaxDouble, ConstantStrings.MaxDouble)]
+        [InlineData(ConstantStrings.MaxIntPlus1, ConstantStrings.MaxIntPlus1)]
+        [InlineData(ConstantStrings.Negative + ConstantStrings.MaxIntPlus1, ConstantStrings.MaxIntPlus1)]
+        [InlineData(ConstantStrings.MaxFloatPlus1, ConstantStrings.MaxFloatPlus1)]
+        [InlineData(ConstantStrings.Negative + ConstantStrings.MaxFloatPlus1, ConstantStrings.MaxFloatPlus1)]
+        [InlineData(ConstantStrings.MaxDoublePlus1, ConstantStrings.MaxDoublePlus1)]
+        [InlineData(ConstantStrings.Negative + ConstantStrings.MaxDoublePlus1, ConstantStrings.MaxDoublePlus1)]
+        public void ShouldGetAbsoluteValue(string value, string expected)
         {
             BigFloat bigValue = new BigFloat(value);
             BigFloat bigExpected = new BigFloat(expected);
