6967930140902679ad7682d49e25d74562684d28
Evan Quan <evanlquan@gmail.com>
2019-09-02 18:10:12 -0600

    Add adding tests.

---
+++ BigFloatingPoint.FunctionalTests/UsingOperators/WhenAdding.cs
@@ -0,0 +1,38 @@
+﻿using BigFloatingPoint.Implementations;
+using Xunit;
+
+namespace BigFloatingPoint.FunctionalTests.UsingOperators
+{
+    public class WhenAdding
+    {
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(1, 1, 2)]
+        [InlineData(-1, -1, -2)]
+        [InlineData(-1.5, -1.5, -3.0)]
+        [InlineData(int.MaxValue, -int.MaxValue, 0)]
+        [InlineData(-int.MaxValue, int.MaxValue, 0)]
+        [InlineData(float.MaxValue, -float.MaxValue, 0)]
+        [InlineData(-float.MaxValue, float.MaxValue, 0)]
+        [InlineData(-double.MaxValue, double.MaxValue, 0)]
+        [InlineData(1, -1, 0)]
+        [InlineData(-1, 1, 0)]
+        [InlineData(1.5, -1.5, 0)]
+        [InlineData(-1.5, 1.5, 0)]
+        public void ShouldAdd(
+            double leftAddend,
+            double rightAddend,
+            double expectedSum)
+        {
+            BigFloat bigLeftAddend = new BigFloat(leftAddend);
+            BigFloat bigRightAddend = new BigFloat(rightAddend);
+
+            BigFloat expectedBigSum = new BigFloat(expectedSum);
+            BigFloat actualBigSum = bigLeftAddend + bigRightAddend;
+
+            Assert.Equal(
+                expected: expectedBigSum,
+                actual: actualBigSum);
+        }
+    }
+}
