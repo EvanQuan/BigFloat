88b1f385f89a12f30003be88d414ebcd775ead04
Evan Quan <evanlquan@gmail.com>
2019-09-02 18:14:15 -0600

    Add absolute value tests.

---
+++ BigFloatingPoint.FunctionalTests/UsingOperators/WhenGettingAbsoluteValue.cs
@@ -0,0 +1,34 @@
+﻿using BigFloatingPoint.Implementations;
+using Xunit;
+
+namespace BigFloatingPoint.FunctionalTests.UsingOperators
+{
+
+    public class WhenGettingAbsoluteValue
+    {
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(-1, 1)]
+        [InlineData(-1.5, 1.5)]
+        [InlineData(1.5, 1.5)]
+        [InlineData(int.MaxValue, int.MaxValue)]
+        [InlineData(-int.MaxValue, int.MaxValue)]
+        [InlineData(float.MaxValue, float.MaxValue)]
+        [InlineData(-float.MaxValue, float.MaxValue)]
+        [InlineData(double.MaxValue, double.MaxValue)]
+        [InlineData(-double.MaxValue, double.MaxValue)]
+        public void ShouldGetAbsoluteValue(double value, double expected)
+        {
+            BigFloat bigValue = new BigFloat(value);
+            BigFloat bigExpected = new BigFloat(expected);
+
+            bigValue = +bigValue;
+
+            Assert.Equal(
+                expected: bigExpected,
+                actual: bigValue);
+        }
+            
+    }
+}
