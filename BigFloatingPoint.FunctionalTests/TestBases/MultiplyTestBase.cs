8343e59e2dc645ddb42aedeb7caac1234287f472
Evan Quan <evanlquan@gmail.com>
2019-09-08 20:39:13 -0600

    Add Multiply test

---
+++ BigFloatingPoint.FunctionalTests/TestBases/MultiplyTestBase.cs
@@ -0,0 +1,49 @@
+﻿using BigFloatingPoint.Implementations;
+using Xunit;
+
+namespace BigFloatingPoint.FunctionalTests.TestBases
+{
+    public abstract class MultiplyTestBase
+    {
+
+        [Theory]
+        [InlineData("1", "1", "1")]
+        [InlineData("-1", "1", "-1")]
+        [InlineData("-1", "-1", "1")]
+        [InlineData("1", "0", "0")]
+        [InlineData("-1", "0", "0")]
+        [InlineData("1", "2", "2")]
+        [InlineData("2", "2", "4")]
+        [InlineData("-2", "2", "-4")]
+        [InlineData("-2", "-2", "4")]
+        [InlineData(ConstantStrings.MaxInt, "2", ConstantStrings.MaxIntTimes2)]
+        [InlineData(ConstantStrings.MaxInt, "-2", ConstantStrings.NegativeMaxIntTimes2)]
+        [InlineData(ConstantStrings.MaxFloat, "2", ConstantStrings.MaxFloatTimes2)]
+        [InlineData(ConstantStrings.MaxFloat, "-2", ConstantStrings.NegativeMaxFloatTimes2)]
+        [InlineData(ConstantStrings.MaxDouble, "2", ConstantStrings.MaxDoubleTimes2)]
+        [InlineData(ConstantStrings.MaxDouble, "-2", ConstantStrings.NegativeMaxDoubleTimes2)]
+        public void ShouldMultiply(
+            string multiplicand,
+            string multiplier,
+            string expected)
+        {
+            BigFloat bigMultiplicand = new BigFloat(multiplicand);
+            BigFloat bigMultiplier = new BigFloat(multiplier);
+            BigFloat actualProduct1 = this.Multiply(bigMultiplicand, bigMultiplier);
+            BigFloat actualProduct2 = this.Multiply(bigMultiplier, bigMultiplicand);
+            BigFloat expectedProduct = new BigFloat(expected);
+
+            Assert.Equal(
+                expected: expectedProduct,
+                actual: actualProduct1);
+
+            Assert.Equal(
+                expected: expectedProduct,
+                actual: actualProduct2);
+        }
+
+        protected abstract BigFloat Multiply(
+            BigFloat multiplicand,
+            BigFloat multiplier);
+    }
+}
---
+++ BigFloatingPoint.FunctionalTests/UsingInstanceMethods/WhenMultiplying.cs
@@ -0,0 +1,16 @@
+﻿using BigFloatingPoint.FunctionalTests.TestBases;
+using BigFloatingPoint.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigFloatingPoint.FunctionalTests.UsingInstanceMethods
+{
+    public class WhenMultiplying : MultiplyTestBase
+    {
+        protected override BigFloat Multiply(BigFloat multiplicand, BigFloat multiplier)
+        {
+            return multiplicand.Multiply(multiplier);
+        }
+    }
+}
