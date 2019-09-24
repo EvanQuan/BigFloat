8f7ab8c41686528b5d30f4677d20ef4fce83e171
Evan Quan <evanlquan@gmail.com>
2019-09-23 20:10:14 -0600

    Add remaining decrement tests.

---
+++ BigFloatingPoint.FunctionalTests/UsingInstanceMethods/WhenDecrementing.cs
@@ -0,0 +1,21 @@
+﻿using BigFloatingPoint.FunctionalTests.TestBases;
+using BigFloatingPoint.Implementations;
+
+namespace BigFloatingPoint.FunctionalTests.UsingInstanceMethods
+{
+    public class WhenDecrementing : DecrementTest
+    {
+        protected override BigFloat PostDecrement(ref BigFloat value)
+        {
+            BigFloat preValue = value;
+            value = value.Decrement();
+            return preValue;
+        }
+
+        protected override BigFloat PreDecrement(ref BigFloat value)
+        {
+            value = value.Decrement();
+            return value;
+        }
+    }
+}
---
+++ BigFloatingPoint.FunctionalTests/UsingStaticMethods/WhenDecrementing.cs
@@ -0,0 +1,24 @@
+﻿using BigFloatingPoint.FunctionalTests.TestBases;
+using BigFloatingPoint.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigFloatingPoint.FunctionalTests.UsingStaticMethods
+{
+    public class WhenDecrementing : DecrementTest
+    {
+        protected override BigFloat PostDecrement(ref BigFloat value)
+        {
+            BigFloat preValue = value;
+            value = BigFloat.Decrement(value);
+            return preValue;
+        }
+
+        protected override BigFloat PreDecrement(ref BigFloat value)
+        {
+            value = BigFloat.Decrement(value);
+            return value;
+        }
+    }
+}
