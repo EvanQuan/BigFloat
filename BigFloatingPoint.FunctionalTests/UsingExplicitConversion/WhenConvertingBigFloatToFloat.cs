c85cdbdda8956ce1daef3b553f90a89cba4aed68
Evan Quan <evanlquan@gmail.com>
2019-09-23 21:34:45 -0600

    Add explicit double conversion tests

---
+++ BigFloatingPoint.FunctionalTests/UsingExplicitConversion/WhenConvertingBigFloatToFloat.cs
@@ -0,0 +1,39 @@
+﻿using BigFloatingPoint.Implementations;
+using System;
+using Xunit;
+
+namespace BigFloatingPoint.FunctionalTests.UsingExplicitConversion
+{
+    public class WhenConvertingBigFloatToFloat
+    {
+        [Theory]
+        [InlineData(Constant.String.MaxFloatPlus1)]
+        [InlineData(Constant.String.MinFloatMinus1)]
+        public void ShouldThrowOverflowException(string value)
+        {
+            BigFloat bigValue = new BigFloat(value);
+
+            Assert.Throws<OverflowException>(() =>
+            {
+                float f = (float)bigValue;
+            });
+        }
+
+        [Theory]
+        [InlineData(Constant.String.MaxFloat, float.MaxValue)]
+        [InlineData(Constant.String.MinFloat, float.MinValue)]
+        [InlineData("-1", -1f)]
+        [InlineData("0", 0f)]
+        [InlineData("1", 1f)]
+        public void ShouldConvert(string value, float expectedValue)
+        {
+            BigFloat bigValue = new BigFloat(value);
+
+            float actualValue = (float)bigValue;
+
+            Assert.Equal(
+                expected: expectedValue,
+                actual: actualValue);
+        }
+    }
+}
---
+++ BigFloatingPoint.FunctionalTests/UsingExplicitConversion/WhenConvertingToDouble.cs
@@ -0,0 +1,40 @@
+﻿using BigFloatingPoint.Implementations;
+using System;
+using Xunit;
+
+namespace BigFloatingPoint.FunctionalTests.UsingExplicitConversion
+{
+    public class WhenConvertingToDouble
+    {
+        [Theory]
+        [InlineData(Constant.String.MaxDoublePlus1)]
+        [InlineData(Constant.String.MinDoubleMinus1)]
+        public void ShouldThrowOverflowException(string value)
+        {
+            BigFloat bigValue = new BigFloat(value);
+
+            Assert.Throws<OverflowException>(() =>
+            {
+                double f = (double)bigValue;
+            });
+        }
+
+        [Theory]
+        [InlineData(Constant.String.MaxDouble, double.MaxValue)]
+        [InlineData(Constant.String.MinDouble, double.MinValue)]
+        [InlineData("-1", -1d)]
+        [InlineData("0", 0d)]
+        [InlineData("1", 1d)]
+        public void ShouldConvert(string value, double expectedValue)
+        {
+            BigFloat bigValue = new BigFloat(value);
+
+            double actualValue = (double)bigValue;
+
+            Assert.Equal(
+                expected: expectedValue,
+                actual: actualValue,
+                precision: 15);
+        }
+    }
+}
